37197aa08831108c5c5007e717bfe9264c4bd2d1
Ajama11 <ajama007@gmail.com>
2026-05-29 10:23:46 -0500

    a

--- AjamaGhouliganCode/Scenes/GhouliganMegaLabel.cs
+++ AjamaGhouliganCode/Scenes/GhouliganMegaLabel.cs
@@ -1,9 +1,17 @@
 using Godot;
 using MegaCrit.Sts2.addons.mega_text;
+using MegaCrit.Sts2.Core.Assets;
 
 namespace AjamaGhouligan.AjamaGhouliganCode.Scenes;
 
 [GlobalClass]
 public partial class GhouliganMegaLabel : MegaLabel
 {
+    private const string MegaLabelFont = "res://themes/kreon_bold_glyph_space_one.tres";
+    
+    public override void _Ready()
+    {
+        AddThemeFontOverride(ThemeConstants.Label.Font, PreloadManager.Cache.GetAsset<Font>(MegaLabelFont));
+        base._Ready();
+    }
 }
---
+++ AjamaGhouliganCode/Scenes/GhouliganNParticlesContainer.cs
@@ -0,0 +1,9 @@
+using Godot;
+using MegaCrit.Sts2.Core.Nodes.Vfx.Utilities;
+
+namespace AjamaGhouligan.AjamaGhouliganCode.Scenes;
+
+[GlobalClass]
+public partial class GhouliganNParticlesContainer : NParticlesContainer
+{
+}
