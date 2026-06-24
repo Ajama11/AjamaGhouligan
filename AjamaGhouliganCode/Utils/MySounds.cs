b75bb7e3502ca8dc3072030d16c6c0c081a019fb
Ajama11 <ajama007@gmail.com>
2026-06-24 08:45:46 -0500

    Add sfx

--- AjamaGhouliganCode/Utils/MySounds.cs
+++ AjamaGhouliganCode/Utils/MySounds.cs
@@ -5,4 +5,6 @@ namespace AjamaGhouligan.AjamaGhouliganCode.Utils;
 public static class MySounds
 {
     public static readonly ModSound GoofPop = new("res://AjamaGhouligan/audio/goof_pop.ogg");
+    
+    public static readonly ModSound Laugh = new("res://AjamaGhouligan/audio/laugh.ogg");
 }
