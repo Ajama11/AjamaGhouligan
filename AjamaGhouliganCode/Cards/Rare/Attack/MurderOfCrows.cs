86be9e26dab9321f9ea26dfae3ebb217ddf342d7
Ajama11 <ajama007@gmail.com>
2026-08-28 14:55:04 -0500

    Replace Vampiric Ward with Murder of Crows

--- AjamaGhouliganCode/Cards/Rare/Attack/MurderOfCrows.cs
+++ AjamaGhouliganCode/Cards/Rare/Attack/MurderOfCrows.cs
@@ -5,47 +5,48 @@ using AjamaGhouligan.AjamaGhouliganCode.Powers;
 using AjamaGhouligan.AjamaGhouliganCode.Utils;
 using BaseLib.Extensions;
 using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Audio.Debug;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.Monsters;
-using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
 
-namespace AjamaGhouligan.AjamaGhouliganCode.Cards.Rare.Power;
+namespace AjamaGhouligan.AjamaGhouliganCode.Cards.Rare.Attack;
 
-public class VampiricWard() : AjamaGhouliganCard(2,
-    CardType.Power, CardRarity.Rare,
-    TargetType.Self)
+public class MurderOfCrows() : AjamaGhouliganCard(1,
+    CardType.Attack, CardRarity.Rare,
+    TargetType.AllEnemies)
 {
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new PowerVar<VampiricWardPower>(3)
+        new DamageVar(9, DamageProps.card),
+        new PowerVar<MurderOfCrowsPower>(1)
     ];
 
     public override IEnumerable<CardKeyword> CanonicalKeywords =>
     [
-        MyEnums.Haunted
-    ];
-    
-    public override BundledHoverTipManager MyBundles =>
-    [
-        BundledHoverTipFactory.FromPower<DoomPower>()
+        MyEnums.Haunted,
+        MyEnums.Grave
     ];
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
-        await CreatureCmd.TriggerAnim(Owner.Creature, "PowerUp", Owner.Character.PowerUpAnimDelay);
+        await CommonActions.CardAttack(this, play,
+                vfx: VfxCmd.bloodyImpactPath,
+                tmpSfx: TmpSfx.daggerThrow)
+            .Execute(choiceContext);
 
-        await CommonActions.ApplySelf<VampiricWardPower>(choiceContext, this);
+        await CommonActions.Apply<MurderOfCrowsPower>(choiceContext,
+            CombatState!.HittableEnemies, this);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Power<VampiricWardPower>().UpgradeValueBy(1);
+        DynamicVars.Power<MurderOfCrowsPower>().UpgradeValueBy(1);
     }
 }
---
+++ AjamaGhouliganCode/Powers/MurderOfCrowsPower.cs
@@ -0,0 +1,50 @@
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace AjamaGhouligan.AjamaGhouliganCode.Powers;
+
+public class MurderOfCrowsPower : AjamaGhouliganPower
+{
+    public override PowerType Type => PowerType.Debuff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override PowerInstanceType InstanceType => PowerInstanceType.InstancedPerApplier;
+
+    public override Task BeforeCardPlayed(CardPlay cardPlay)
+    {
+        if (Applier?.Player == null) return Task.CompletedTask;
+        if (cardPlay.Card.Owner != Applier.Player) return Task.CompletedTask;
+        
+        GetInternalData<Data>().AmountsForPlayedCards.Add(cardPlay.Card, Amount);
+
+        return Task.CompletedTask;
+    }
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (!GetInternalData<Data>().AmountsForPlayedCards.Remove(cardPlay.Card, out var amount)) return;
+        
+        Flash();
+
+        await CreatureCmd.Damage(choiceContext, Owner, amount, DamageProps.nonCardHpLoss, null, null);
+    }
+
+    public override async Task AfterSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
+    {
+        if (!participants.Contains(Owner)) return;
+
+        await PowerCmd.Remove(this);
+    }
+
+    protected override object InitInternalData() => new Data();
+    public class Data
+    {
+        public readonly Dictionary<CardModel, int> AmountsForPlayedCards = [];
+    }
+}
