f2a80bb5c2fa8b529c294fc78320d6194d01d2a6
Ajama11 <ajama007@gmail.com>
2026-09-25 08:42:24 -0500

    Nerf Jared to work every 2 turns instead of every turn

--- AjamaGhouliganCode/Relics/Jared.cs
+++ AjamaGhouliganCode/Relics/Jared.cs
@@ -6,9 +6,12 @@ using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Helpers;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Nodes.CommonUi;
+using MegaCrit.Sts2.Core.Rooms;
+using MegaCrit.Sts2.Core.Saves.Runs;
 
 namespace AjamaGhouligan.AjamaGhouliganCode.Relics;
 
@@ -16,11 +19,43 @@ public class Jared() : AjamaGhouliganRelic
 {
     public override RelicRarity Rarity =>
         RelicRarity.Rare;
+
+    public override bool ShowCounter => true;
+    public override int DisplayAmount =>
+        IsActivating ? DynamicVars[Turns].IntValue : TurnsSeen;
+
+    public const int TurnsAmount = 2;
+    public const string Turns = "Turns";
     
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new TreatVar(1)
+        new TreatVar(1),
+        new IntVar(Turns, TurnsAmount)
     ];
+
+    [SavedProperty]
+    public int TurnsSeen
+    {
+        get;
+        set
+        {
+            AssertMutable();
+            field = value;
+            InvokeDisplayAmountChanged();
+        }
+    }
+
+    [SavedProperty]
+    public bool IsActivating
+    {
+        get;
+        set
+        {
+            AssertMutable();
+            field = value;
+            InvokeDisplayAmountChanged();
+        }
+    }
 
     protected override IEnumerable<IHoverTip> ExtraHoverTips =>
     [
@@ -31,12 +66,33 @@ public class Jared() : AjamaGhouliganRelic
     {
         if (player != Owner) return;
         
-        Flash();
+        TurnsSeen = (TurnsSeen + 1) % DynamicVars[Turns].IntValue;
+        Status = TurnsSeen == DynamicVars[Turns].IntValue - 1 ?
+            RelicStatus.Active :
+            RelicStatus.Normal;
+        
+        if (TurnsSeen != 0) return;
+        
+        _ = TaskHelper.RunSafely(DoActivateVisuals());
         
         await MyActions.CreateTreats(DynamicVars.Treat.IntValue, Owner, combatState, modifyCardsBeforePreview: list =>
         {
             CardCmd.Upgrade(list, CardPreviewStyle.None);
             return list;
         });
+    }
+
+    public override Task AfterCombatEnd(CombatRoom _)
+    {
+        Status = RelicStatus.Normal;
+        return Task.CompletedTask;
+    }
+
+    public async Task DoActivateVisuals()
+    {
+        IsActivating = true;
+        Flash();
+        await Cmd.Wait(1);
+        IsActivating = false;
     }
 }
